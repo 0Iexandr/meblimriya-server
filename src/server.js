ac6b9b9cf77a6a409fa68385347e8eb5a0ca806d
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-12-23 02:16:39 +0200

    updated database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
+
 const express = require('express');
-const router = express.Router();
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/database');
 
+const PORT = process.env.PORT || 5000;
 // const corsOptions = {
 //   origin: ['https://meblimriya.com', 'https://www.meblimriya.com'],
 //   credentials: true
